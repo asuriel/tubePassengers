ebb1fcc954e854a1305fe21493c99f55665c6b49
Aurora <a.suriel@yahoo.co.uk>
2015-11-09 22:56:46 +0000

    Cleaning up chart.js

--- chart.js
+++ chart.js
@@ -5,74 +5,51 @@ function radius(d) { return d.passengerNumber; }
 function color(d) { return d.line; }
 function key(d) { return d.name; }
 
-
-//line colours
-var lineColors = function(){
-    var tubeColors = ["#000099","#FFCC00","#000000","#CC3333","#0099CC"];
-    return d3.scale.ordinal().range(tubeColors);
-};
-
 // Chart dimensions.
-var margin = {top: 0, right: 0, bottom: 0, left: 0},
-    width = 960 - margin.right ,
-    height = 500 - margin.top - margin.bottom ;
+var width = 960,
+    height = 500;
 
+//Creating a d3 scale for the line colours
+var tubeColourScale = function(){
+    var tubeColours = ["#000099","#FFCC00","#000000","#CC3333","#0099CC"];
+    var lineNames =  ["Picadilly","Circle","Northern","Central","Victoria"];
+    return d3.scale.ordinal().domain(lineNames).range(tubeColours);
+};
 
 // Various scales. These domains make assumptions of data, naturally.
 var xScale = d3.scale.linear().domain([0,1043]).range([0, width]), 
     yScale = d3.scale.linear().domain([0,747]).range([height, 0]),
-    radiusScale = d3.scale.sqrt().domain([0, 5e4]).range([0, 30]),
-    colorScale = lineColors();
-
-// The x & y axes.
-var xAxis = d3.svg.axis().orient("bottom").scale(xScale).ticks(30, d3.format(",d")),
-    yAxis = d3.svg.axis().scale(yScale).orient("left").ticks(30, d3.format("d"));
-
-//create a second svg container
-var svg2 = d3.select("#sidebar").append("svg")
-  .attr("width", 150)
-  .attr("height", 750)
-  .append("g")
-    // .attr("transform", "translate(" + margin.left + "," + margin.top + ")");    
+    radiusScale = d3.scale.sqrt().domain([0, 5e4]).range([1, 30]),
+    colourScale = tubeColourScale();
 
 // Create the SVG container and set the origin.
-var svg = d3.select("#chart").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom + 250)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var tubeMapSvg = d3.select("#chart").append("svg")
+    .attr("width", width)
+    .attr("height", height + 250)
+  .append("g");   
 
-// Add the x-axis.
-svg.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + height + ")")
-    .attr("visibility", "hidden")
-    .call(xAxis);
+//create a second svg container
+var busyStationSvg = d3.select("#sidebar").append("svg")
+  .attr("width", 150)
+  .attr("height", 750)
+  .append("g");
 
-// Add the y-axis.
-svg.append("g")
-    .attr("class", "y axis")
-    .attr("visibility", "hidden")
-    .call(yAxis);    
-
-//Add the year label; the value is set on transition.
-var label = svg.append("text")
+//Add the time label; the value is set on transition.
+var timeLabel = tubeMapSvg.append("text")
     .attr("class", "year label")
     .attr("text-anchor", "end")
     .attr("y", height)
     .attr("x", width )
     .text(285);
 
-
-var numberOfStationsToDisplay = 8;
-
+//Create labels for the top # busiest stations
 function createBusyStationLabels(numberOfTopStations) {
 
   var busyStationLabels = [];
 
   for(var i = 0; i < numberOfTopStations; i++) {
 
-    busyStationLabels[i] = svg2.append("text")
+    busyStationLabels[i] = busyStationSvg.append("text")
       .attr("y", i*20)
       .attr("x", 0)
       .text(i+1 + "st  " + "busiest");
@@ -81,33 +58,34 @@ function createBusyStationLabels(numberOfTopStations) {
   return busyStationLabels;
 }
 
-var busyStationLabels = createBusyStationLabels(numberOfStationsToDisplay);
+var numberOfStationsToDisplay = 8;
 
+var busyStationLabels = createBusyStationLabels(numberOfStationsToDisplay);
 
 
 
 
 d3.json("/data/ready_data.json", function(stations) {
 
-     // Add a dot per station. Initialize the data at 1800, and set the colors.
-    var dot = svg.append("g")
+     // Add a dot per station. Initialize the data at 04:45(285), and set the colors.
+    var dot = tubeMapSvg.append("g")
         .attr("class", "dots")
-    .selectAll(".dot")
-        .data(getData(285))
-    .enter().append("circle")
+        .selectAll(".dot")
+        .data(getStationDataAt(285))
+        .enter().append("circle")
         .attr("class", "dot")
-        .style("fill", function(d) { return colorScale(color(d)); })
+        .style("fill", function(d) { return colourScale(color(d)); })
         .call(position)
         .sort(order);
 
-    // Add a title.
+    // Add a tooltip.
     dot.append("title")
-       .text(function(d) { return d.name; });
+       .text(function(d) { return d.name;});
 
-  // Add an overlay for the year label.
-  var box = label.node().getBBox();
+  // Add an overlay for the time label to control the time.
+  var box = timeLabel.node().getBBox();
 
-  var overlay = svg.append("rect")
+  var overlay = tubeMapSvg.append("rect")
         .attr("class", "overlay")
         .attr("x", box.x)
         .attr("y", box.y)
@@ -116,7 +94,7 @@ d3.json("/data/ready_data.json", function(stations) {
         .on("mouseover", enableInteraction);
 
   // Start a transition that interpolates the data based on time.
-  svg.transition()
+  tubeMapSvg.transition()
       .duration(30000)
       .ease("linear")
       .tween("times", tweenTimes)
@@ -142,7 +120,7 @@ d3.json("/data/ready_data.json", function(stations) {
             .clamp(true);
 
         // Cancel the current transition, if any.
-        svg.transition().duration(0);
+        tubeMapSvg.transition().duration(0);
 
         overlay
             .on("mouseover", mouseover)
@@ -151,11 +129,11 @@ d3.json("/data/ready_data.json", function(stations) {
             .on("touchmove", mousemove);
 
         function mouseover() {
-            label.classed("active", true);
+            timeLabel.classed("active", true);
         }
 
         function mouseout() {
-            label.classed("active", false);
+            timeLabel.classed("active", false);
         }
 
         function mousemove() {
@@ -163,7 +141,7 @@ d3.json("/data/ready_data.json", function(stations) {
         }
     }
 
-      // Tweens the entire chart by first tweening the year, and then the data.
+      // Tweens the entire chart by first tweening the time, and then the data.
   // For the interpolated data, the dots and label are redrawn.
     function tweenTimes() {
       var times = d3.interpolateNumber(285, 1425);
@@ -171,34 +149,32 @@ d3.json("/data/ready_data.json", function(stations) {
     }
 
 
-    function renderBusiestStations(stationData) {
-
+    function updateBusiestStations(stationData) {
       var sortedData =_.sortBy(stationData,"passengerNumber").reverse();
 
        for (var i = 0; i < busyStationLabels.length; i++) {
+         busyStationLabels[i].text(sortedData[i].name); 
+       }
+    }
 
-         busyStationLabels[i].text(sortedData[i].name);
-       
-       };
-        // busy1.text(sortedData[0].name);
-        // busy2.text(sortedData[1].name);
-        // busy3.text(sortedData[2].name);
+    function updateDots(data) {
+      dot.data(data, key).call(position);
+    }
+
+    function updateTimeLabel(time){
+      timeLabel.text( getTime( (Math.round(time) * 60 * 1000)));
     }
     
    // Updates the display to show time of the day.
     function updateChart(time) {
-      var data = getData(time);
+      var data = getStationDataAt(time);
 
-      renderBusiestStations(data); 
-      dot.data(data, key).call(position);//.sort(order);
-      //label.text(Math.floor(285/60) + ":" + 285%60);
-      label.text( getTime( (Math.round(time) * 60 * 1000)));
-
-
-    //console.log('time update:' + time);
+      updateBusiestStations(data); 
+      updateDots(data);
+      updateTimeLabel(time);
     }
 
-    function getData(time) {
+    function getStationDataAt(time) {
         return stations.map(function(d) {
             return {
                 name: d.name,
@@ -222,11 +198,7 @@ d3.json("/data/ready_data.json", function(stations) {
        var date = new Date(milliseconds);
        return date.getHours() + ":" + (date.getMinutes() <= 9 ? "0" + date.getMinutes() : date.getMinutes())
 
-    }
-
-
-
-    
+    }   
 
 });    
 
